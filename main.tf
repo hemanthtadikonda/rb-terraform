7fdbd6b6e42b452a3c6a7579ad46a96dd4f72e59
hemanth <hemanthtadikonda29@gmail.com>
2023-11-17 17:02:18 +0530

    pc -vpc

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ module "vpc" {
   cidr = each.value["cidr"]
   subnets= each.value ["subnets"]
   default_vpc_id = var.default_vpc_id
+  default_vpc_cidr_block = var.default_vpc_cidr_block
   def_route_table_id =var.def_route_table_id
 
 
