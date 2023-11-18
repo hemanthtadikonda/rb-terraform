a76626e055c972625032a9d82e0b59bc45ec1da8
hemanth <hemanthtadikonda29@gmail.com>
2023-11-18 09:00:16 +0530

    pc -vpc

--- main.tf
+++ main.tf
@@ -1,15 +1,17 @@
 module "vpc" {
   source = "git::https://github.com/hemanthtadikonda/rb-tf-vpc.git"
-  for_each = var.vpc
-
-  cidr = each.value["cidr"]
-  subnets= each.value ["subnets"]
-
   env = var.env
   tags = var.tags
   default_vpc_id = var.default_vpc_id
   default_vpc_cidr_block = var.default_vpc_cidr_block
   def_route_table_id =var.def_route_table_id
+
+  for_each = var.vpc
+
+  cidr = each.value["cidr"]
+  subnets= each.value ["subnets"]
+
+
 
 }
 
