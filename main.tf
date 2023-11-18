49650e6408e163b5f65f3cdec85370ebc497c7db
hemanth <hemanthtadikonda29@gmail.com>
2023-11-18 09:31:03 +0530

    pc -vpc

--- main.tf
+++ main.tf
@@ -12,21 +12,21 @@ module "vpc" {
   def_route_table_id =var.def_route_table_id
 
 }
-
-module "alb" {
-  source = "git::https://github.com/hemanthtadikonda/rb-tf-alb.git"
-  for_each = var.alb
-
-  internal = each.value["internal"]
-  lb_type  = each.value["lb_type"]
-  sg_port  = each.value["sg_port"]
-  sg_ingress_cidr = each.value["sg_ingress_cidr"]
-  vpc_id = each.value["internal"] ? local.vpc_id : var.default_vpc_id
-  subnets = each.value["internal"] ? local.app_subnets : data.aws_subnets.main.ids
-
-  env = var.env
-  tags = var.tags
-
-
-
-}
+#
+#module "alb" {
+#  source = "git::https://github.com/hemanthtadikonda/rb-tf-alb.git"
+#  for_each = var.alb
+#
+#  internal = each.value["internal"]
+#  lb_type  = each.value["lb_type"]
+#  sg_port  = each.value["sg_port"]
+#  sg_ingress_cidr = each.value["sg_ingress_cidr"]
+#  vpc_id = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+#  subnets = each.value["internal"] ? local.app_subnets : data.aws_subnets.main.ids
+#
+#  env = var.env
+#  tags = var.tags
+#
+#
+#
+#}
