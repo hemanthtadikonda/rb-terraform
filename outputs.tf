d7bc6edba4343403ea9a95ef834c02f5e9d7a3ef
hemanth <hemanthtadikonda29@gmail.com>
2023-11-28 07:31:12 +0530

    pc - "apps"

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,18 @@
-output "subnets" {
-  value = module.vpc
-}
+#output "subnets" {
+#  value = module.vpc
+#}
+#
+#output "vpc_id_test" {
+#  value = local.vpc_id
+#}
+#output "def-sub" {
+#  value = data.aws_subnets.main.ids
+#}
+#
+#output "app-subnets" {
+#  value = local.app_subnets
+#}
 
-output "vpc_id_test" {
-  value = local.vpc_id
-}
-output "def-sub" {
-  value = data.aws_subnets.main.ids
-}
-
-output "app-subnets" {
-  value = local.app_subnets
+output "lb_details" {
+  value = module.alb
 }
