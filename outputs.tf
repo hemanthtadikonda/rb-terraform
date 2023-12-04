7cfb31d40716799492c819aac307283880f92073
hemanth <hemanthtadikonda29@gmail.com>
2023-12-04 18:29:50 +0530

    pc - "apps"

--- outputs.tf
+++ outputs.tf
@@ -13,24 +13,28 @@
 #  value = local.app_subnets
 #}
 
-output "lb_details" {
-  value = module.alb
+output "app-subnets" {
+  value = local.db_subnets
 }
 
-output "lb_int_dns" {
-  value = lookup(lookup(lookup(module.alb , "internal" , null) , "alb" ,null), "dns_name",null)
-}
-output "lb_int_listener" {
-  value = lookup(lookup(lookup(module.alb , "internal" ,null ), "lb_listener" , null) , "arn" , null )
-}
-
-output "lb_pub_dns" {
-  value = lookup(lookup(lookup(module.alb , "public" , null) , "alb" ,null), "dns_name",null)
-}
-output "lb_pub_listener" {
-  value = lookup(lookup(lookup(module.alb , "public" ,null ), "lb_listener" , null) , "arn" , null )
-}
-
-output "private_lb_add_details" {
-  value = data.dns_a_record_set.private_lb_add.addrs
-}
+#output "lb_details" {
+#  value = module.alb
+#}
+#
+#output "lb_int_dns" {
+#  value = lookup(lookup(lookup(module.alb , "internal" , null) , "alb" ,null), "dns_name",null)
+#}
+#output "lb_int_listener" {
+#  value = lookup(lookup(lookup(module.alb , "internal" ,null ), "lb_listener" , null) , "arn" , null )
+#}
+#
+#output "lb_pub_dns" {
+#  value = lookup(lookup(lookup(module.alb , "public" , null) , "alb" ,null), "dns_name",null)
+#}
+#output "lb_pub_listener" {
+#  value = lookup(lookup(lookup(module.alb , "public" ,null ), "lb_listener" , null) , "arn" , null )
+#}
+#
+#output "private_lb_add_details" {
+#  value = data.dns_a_record_set.private_lb_add.addrs
+#}
